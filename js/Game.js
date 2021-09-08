419037bbe3b17e0466991e3bc92098e86ac9a7cd
willstutely <jonathanandrewdrake@gmail.com>
2021-09-08 18:50:34 -0500

    started work on game and phrase objects

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+/**
+ * Create phrases for use in game
+ * @return {array} An array of phrases that could be used in the game
+ */
+
+    createPhrases() {
+        const phraseArray = [
+            "Never give up never surrender",
+            "As you wish",
+            "Slow down you move too fast",
+            "The stuff that dreams are made of",
+            "Bridge over troubled water"
+        ];
+        return phraseArray;
+    };
+
+/**
+ * Selects random phrase from phrases property
+ * @return {Object} Phrase object chosen to be used
+ */
+
+    getRandomPhrase() {
+        const randomNumber = Math.ceil(Math.random() * this.phrases.length) -1;
+        return this.phrases[randomNumber];
+    };
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
